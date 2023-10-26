bdfa454f2b729c5ea64bf761f973965cf35d8e45
Jhonson <hbiblia@gmail.com>
2023-10-26 19:17:54 -0400

    Slow web no append

--- youtube-jump/content-scripts/content.js
+++ youtube-jump/content-scripts/content.js
@@ -3,6 +3,7 @@ let url_hitory, url_history_current;
 let url_change = false
 let url_check_time = 1000
 let iframe_paste = false
+let iframe_id = "custom_y"
 
 let youtube_jump = function () {
   let i = setInterval(function () {
@@ -12,6 +13,10 @@ let youtube_jump = function () {
 
       if (url_hitory != url_history_current) url_change = true;
 
+      if ($(`#${iframe_id}`) == null){
+        iframe_paste = false
+      }
+
       if (url_change) {
         url_change = false
         url_history_current = url_hitory
@@ -19,10 +24,10 @@ let youtube_jump = function () {
 
         if (!iframe_paste) {
           iframe_paste = true
-          $('#player').append(`<iframe id="custom_y" style="width:100%; height:100%; z-index:99; position:absolute; top:0;left:0;" src="https://www.youtube-nocookie.com/embed/${url_code}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
+          $('#player').append(`<iframe id="${iframe_id}" style="width:100%; height:100%; z-index:99; position:absolute; top:0;left:0;" src="https://www.youtube-nocookie.com/embed/${url_code}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
         }
 
-        $('#custom_y').attr('src', `https://www.youtube-nocookie.com/embed/${url_code}`)
+        $(`#${iframe_id}`).attr('src', `https://www.youtube-nocookie.com/embed/${url_code}`)
       }
     }
   }, url_check_time);
@@ -34,6 +39,6 @@ $(function () {
     let e = $('#player')
     let w = e.width()
     let h = e.height()
-    $('#custom_y').css({ 'width': w + 'px', 'height': h + 'px' })
+    $(`#${iframe_id}`).css({ 'width': w + 'px', 'height': h + 'px' })
   })
 });
