8870912dea1b147666a33ed9b194a26fa23242ee
Jhonson <hbiblia@gmail.com>
2023-10-27 08:59:44 -0400

    if paused

--- youtube-jump/content-scripts/content.js
+++ youtube-jump/content-scripts/content.js
@@ -1,6 +1,7 @@
 
 let url_hitory, url_history_current;
 let url_change = false
+let video_youtube = null
 const url_check_time = 1000
 const iframe_id_str = btoa(crypto.randomUUID())
 const iframe_id = "#" + iframe_id_str
@@ -10,28 +11,30 @@ let youtube_jump = function () {
     if ($('#player') != null && location.search != '') {
       // clearInterval(i)
 
+      if (document.querySelector('.html5-main-video').paused == false) {
+        document.querySelector('.html5-main-video').pause();
+        document.querySelector('.html5-main-video').muted = true
+        document.querySelector('.html5-main-video').style.display = 'none'
+      }
+
       // para el cambio de la url
       url_hitory = location.search
       if (url_hitory != url_history_current) url_change = true;
-      
+
       // cuando aun no tenemos el custom_youtube
       if (document.querySelector(iframe_id) == null) {
         $('#player').append(`<iframe id="${iframe_id_str}" style="width:100%; height:100%; z-index:99; position:absolute; top:0;left:0;" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
         url_change = true
       }
-      
+
       // Cuando cambiamos de video
       if (url_change) {
         url_change = false
         url_history_current = url_hitory
         let url_code = location.search.split('=')[1].split('&')[0]
-        
+
         $(iframe_id).attr('src', `https://www.youtube-nocookie.com/embed/${url_code}`)
         url_empty = false
-        setTimeout(function(){
-          document.querySelector('.html5-main-video').pause();
-          document.querySelector('.html5-main-video').style = 'display:none'
-        },200)
       }
     }
 
@@ -45,6 +48,7 @@ let youtube_jump = function () {
 
 $(function () {
 
+  video_youtube = document.querySelector('.html5-main-video')
   youtube_jump()
 
   $(window).on('resize', function (event) {
