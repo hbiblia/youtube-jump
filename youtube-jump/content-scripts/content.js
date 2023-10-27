18b2256cd403c642d50393bb2625443120b772a7
Jhonson <hbiblia@gmail.com>
2023-10-26 20:29:41 -0400

    0.0.0.5

--- youtube-jump/content-scripts/content.js
+++ youtube-jump/content-scripts/content.js
@@ -9,15 +9,18 @@ let youtube_jump = function () {
   let i = setInterval(function () {
     if ($('#player') != null && location.search != '') {
       // clearInterval(i)
-      url_hitory = location.search
 
+      // para el cambio de la url
+      url_hitory = location.search
       if (url_hitory != url_history_current) url_change = true;
 
+      // cuando aun no tenemos el custom_youtube
       if (document.querySelector(iframe_id) == null) {
         $('#player').append(`<iframe id="${iframe_id_str}" style="width:100%; height:100%; z-index:99; position:absolute; top:0;left:0;" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
         url_change = true
       }
 
+      // Cuando cambiamos de video
       if (url_change) {
         url_change = false
         url_history_current = url_hitory
@@ -28,6 +31,7 @@ let youtube_jump = function () {
       }
     }
 
+    // Estamos en la pagina principal
     if (location.search == '' && url_change == false) {
       url_change = true
       $(iframe_id).attr('src', ``)
@@ -37,10 +41,12 @@ let youtube_jump = function () {
 
 $(function () {
   youtube_jump()
+
   $(window).on('resize', function (event) {
     let e = $('#player')
     let w = e.width()
     let h = e.height()
     $(iframe_id).css({ 'width': w + 'px', 'height': h + 'px' })
   })
+
 });
